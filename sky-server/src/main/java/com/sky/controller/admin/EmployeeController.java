51bea26fd019de98d001e38eb57bc7f3097e430d
lml <15137628639@163.com>
2025-11-16 00:06:19 +0800

    "Day02-03 新增员工"

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -71,4 +72,13 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * @param employeeDTO
+     * @return Result
+     */
+    @PostMapping
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.insert(employeeDTO);
+        return Result.success();
+    }
 }
