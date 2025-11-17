e51ad071d3b25caaf2b0d77866804140a03700c0
lml <15137628639@163.com>
2025-11-17 13:50:21 +0800

    "Day02-09 员工分页查询"

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,18 +3,17 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -80,5 +79,17 @@ public class EmployeeController {
     public Result save(@RequestBody EmployeeDTO employeeDTO){
         employeeService.insert(employeeDTO);
         return Result.success();
+    }
+
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO url地址栏中的三个参数直接封装成一个对象 无需@RequestBody
+     * name page pagenumber pagesize
+     * @return Result
+     */
+    @GetMapping("/page")
+    public Result page(EmployeePageQueryDTO employeePageQueryDTO){
+        PageResult pageResult = employeeService.queryPage(employeePageQueryDTO);
+        return Result.success(pageResult);
     }
 }
